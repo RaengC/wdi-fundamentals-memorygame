2a3075964115facb122b3b57b047fd9c697bf184
Raeng <raengc@yahoo.com>
2020-05-03 13:32:13 +1000

    Update! Getting there

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -11,14 +11,10 @@ cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 
-
-//check if two matching cards have been played, add logic here
-function checkForMatch () {
-    if (cardsInPlay.length === 2) {
-        if (cardsInPlay[0] === cardsInPlay[1]) {
-            console.log("You found a match!");
-        } else {
-            console.log('Sorry, try again')
-        }
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+         alert("You found a match!");
+    } else {
+        alert('Sorry, try again.')
     }
 }
