86bcf43e275c300e873179000758f7aecd2be43d
Raeng Castagna <raengc@yahoo.com>
2020-05-06 17:43:39 +1000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,26 @@
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [ {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+}, 
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank:"king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{    
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
+
 let cardsInPlay = []; 
 
 function checkForMatch () {
@@ -11,17 +32,23 @@ function checkForMatch () {
 }
 
 function flipCard (cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     console.log(checkForMatch);
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
              alert("You found a match!");
         } else {
             alert('Sorry, try again.')
-        }     
+        } 
+  
     }
 }
 flipCard(0);
 flipCard(2);
+
+  
+
 
